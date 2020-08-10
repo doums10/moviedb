cac616bdb4cf98d0b31dfff16e62dd4293871860
mamadou sylla <m27sylla@gmail.com>
2020-08-10 22:07:23 +0200

    Updates

---
+++ precache-manifest.ea622766d4ccccf80021efc2d4dc08ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e09e0f4f9ea0fc02b6074d36b014e4",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "23a13c50ffeb080f5c8b",
+    "url": "/moviedb/static/css/main.7e47d454.chunk.css"
+  },
+  {
+    "revision": "99d0ffb60db12e2458df",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a13c50ffeb080f5c8b",
+    "url": "/moviedb/static/js/main.6d55aae6.chunk.js"
+  },
+  {
+    "revision": "dff7d7152d1d72c996c0",
+    "url": "/moviedb/static/js/runtime-main.c7335139.js"
+  }
+]);
